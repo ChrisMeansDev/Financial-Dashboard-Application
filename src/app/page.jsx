685aac0e30eabb942b058a4d0f02891ce5d925d7
Chris Means <chris@example.com>
2026-01-27 14:14:27 -0600

    Mini-Lesson 25.7: optimize fonts with next/font and images with next/image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,15 @@
-// import AcmeLogo from '@/app/ui/acme-logo';
+import Image from 'next/image';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import InvoiceList from './ui/invoices/list';
-import styles from './ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      {/* Header */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        {/* Header */}
       </div>
 
-      {/* Main Content */}
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        {/* Left Panel */}
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
@@ -31,17 +26,17 @@ export default function Page() {
             <span>Log in</span>
             <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
-
-          {/* Invoice List */}
-          <div className="mt-8">
-            <h2 className="mb-4 text-lg font-semibold text-gray-700">Invoices</h2>
-            <InvoiceList />
-          </div>
         </div>
 
-        {/* Right Panel */}
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
